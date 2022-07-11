fe9f9be3dc15f41cbeba36f6eba568705335ad43
Archan6el <myk0675@outlook.com>
2022-07-11 14:23:02 -0500

    Name change

---
+++ codedaperture-randomcount.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt
+import random
+
+#the mask is static
+mask = [1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1, 1]
+count = []
+
+xaxis = []
+
+for x in range(1, 44):
+    xaxis.append(str(x))
+
+#fig = plt.figure()
+plt.rcParams["figure.figsize"] = (20,5)
+plt.bar(xaxis, mask)
+plt.title("Mask")
+plt.show()
