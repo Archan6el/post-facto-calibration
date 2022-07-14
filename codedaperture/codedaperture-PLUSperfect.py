0f66fb1cf81187e076bec661f509eb0b9c7681bf
Archan6el <91164464+Archan6el@users.noreply.github.com>
2022-07-14 11:23:44 -0500

    Update codedaperture-PLUSperfect.py

--- codedaperture/codedaperture-PLUSperfect.py
+++ codedaperture/codedaperture-PLUSperfect.py
@@ -4,56 +4,44 @@ import random
 
 #the mask is static of 70 zeros and ones
 mask = [1, 1, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 1, 0, 1, 0, 0, 0, 1, 1, 0, 1, 1, 1, 0, 1, 1, 0, 0, 0, 1, 1, 1, 0, 1, 0, 0, 0, 1, 0, 1, 1]
+#for this case, the count or cast "shadow" is an exact shadow of the mask
 count = []
+#Contains our extrapolated mask
+extrapMask = []
 
-#Fills our count array perfectly. 10 signifies no x-ray hits
-#100 signifies an x-ray hit
+#Extrapolates our mask
 for x in range(400):
     indexOfMask = int(x * 70 / 400)
     if(indexOfMask == 0):
         indexOfMask = 1
     if mask[indexOfMask] == 0:
-        count.append(10)
+        extrapMask.append(10)
     else:
-        count.append(100)
+        extrapMask.append(100)
 
-#Randomly chooses to shift the count array left or right by varying degrees
-direction = random.choice(range(0, 2))
-if direction == 1:
-    x = random.choice(range(len(count)))
-    count = np.pad(count, (x, 0), mode='constant')[:-x]
-else:
-    x = random.choice(range(len(count)))
-    count = np.pad(count, (0, x), mode='constant')[x:]
+#Extrapolated Mask. In this case, since the shadow *is* perfectly aligned with the mask
+#it would be identical to our extrapolated mask
+count = extrapMask
 
 #Our cross correlation array
-cc = np.correlate(mask, count, "full")
+cc = np.correlate(extrapMask, count, mode='full')
 
-
-
-bottom = 0 
-width = 0.8 
-#Used only for the x axis of our mask and count graphs
+#X axes of graphs
 xaxisForMask = np.arange(0, len(mask), 1)
-#for x in range(len(mask)):
-#    xaxisForMask.append(str(x + 1))
-xaxisForCount = []
-for x in range(len(count)):
-    xaxisForCount.append(str(x + 1))
-#Used only for the x axis of our cross correlation graph
-xaxiscc = []
-for x in range(len(cc) ):
-    xaxiscc.append(str(x + 1))
+xaxisForCount = np.arange(0, len(count), 1)
+xaxiscc = np.arange(0, len(cc), 1)
+
 #Everything after this is plotting our graphs
 figure, axis = plt.subplots(3)
+
 axis[0].bar(xaxisForMask, mask)
 axis[0].set_title("Mask")
+
 axis[1].bar(xaxisForCount, count)
 axis[1].set_title("Perfect Counts")
+
 axis[2].bar(xaxiscc, cc)
 axis[2].set_title("Cross Correlation")
-axis[2].tick_params(axis='x', rotation=65)
-
 
 figure.tight_layout()
 figure.set_figheight(5)
