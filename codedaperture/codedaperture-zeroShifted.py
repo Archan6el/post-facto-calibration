71535fb75e62ce81c573b8fd95e0f8c34e4ab22f
Archan6el <91164464+Archan6el@users.noreply.github.com>
2022-07-14 11:25:36 -0500

    Update codedaperture-zeroShifted.py

--- codedaperture/codedaperture-zeroShifted.py
+++ codedaperture/codedaperture-zeroShifted.py
@@ -2,48 +2,51 @@ import matplotlib.pyplot as plt
 import numpy as np
 import random
 
-#the mask is static
-mask = [1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1, 1]
-#for this case, the count or cast "shadow" is an exact shadow of the mask, but shifted
+#the mask is static of 70 zeros and ones
+mask = [1, 1, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 1, 0, 1, 0, 0, 0, 1, 1, 0, 1, 1, 1, 0, 1, 1, 0, 0, 0, 1, 1, 1, 0, 1, 0, 0, 0, 1, 0, 1, 1]
+#for this case, the count or cast "shadow" is an exact shadow of the mask but shifted
 count = []
+#Contains our extrapolated mask
+extrapMask = []
 
-#Fills our count array perfectly. 10 signifies no x-ray hits
-#100 signifies an x-ray hit
-for x in range(len(mask) - len(count)):
-    if mask[x] == 0:
-        count.append(10)
+#Extrapolates our mask
+for x in range(400):
+    indexOfMask = int(x * 70 / 400)
+    if(indexOfMask == 0):
+        indexOfMask = 1
+    if mask[indexOfMask] == 0:
+        extrapMask.append(10)
     else:
-        count.append(100)
+        extrapMask.append(100)
+
+#In this case, the shadow is perfectly aligned with the mask, so it would
+#be identical to our extrapolated mask
+count = extrapMask
 
 #Randomly chooses to shift the count array left or right by varying degrees
 direction = random.choice(range(0, 2))
 if direction == 1:
-    x = random.choice(range(len(mask)))
+    x = random.choice(range(len(count)))
     count = np.pad(count, (x, 0), mode='constant')[:-x]
 else:
-    x = random.choice(range(len(mask)))
+    x = random.choice(range(len(count)))
     count = np.pad(count, (0, x), mode='constant')[x:]
 
 #Our cross correlation array
-cc = np.correlate(mask, count, "full")
+cc = np.correlate(extrapMask, count, "full")
 
-#Used only for the x axis of our mask and count graphs
-xaxis = []
-for x in range(len(mask)):
-    xaxis.append(str(x + 1))
-
-#Used only for the x axis of our cross correlation graph
-xaxiscc = []
-for x in range(len(cc)):
-    xaxiscc.append(str(x + 1))
+#X axes of graphs
+xaxisForMask = np.arange(0, len(mask), 1)
+xaxisForCount = np.arange(0, len(count), 1)
+xaxiscc = np.arange(0, len(cc), 1)
 
 #Everything after this is plotting our graphs
 figure, axis = plt.subplots(3)
 
-axis[0].bar(xaxis, mask)
+axis[0].bar(xaxisForMask, mask)
 axis[0].set_title("Mask")
 
-axis[1].bar(xaxis, count)
+axis[1].bar(xaxisForCount, count)
 axis[1].set_title("Shifted Counts")
 
 axis[2].bar(xaxiscc, cc)
