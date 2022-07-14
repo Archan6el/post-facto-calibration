ca759bea534ab85dfa06cc9e743f3ef6cf399379
Archan6el <91164464+Archan6el@users.noreply.github.com>
2022-07-14 11:24:58 -0500

    Update codedaperture-randomcount.py

--- codedaperture/codedaperture-randomcount.py
+++ codedaperture/codedaperture-randomcount.py
@@ -2,42 +2,47 @@ import matplotlib.pyplot as plt
 import numpy as np
 import random
 
-#the mask is static
-mask = [1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1, 1]
-#for this case, the count or cast "shadow" is random or unrelated to the mask
+#the mask is static of 70 zeros and ones
+mask = [1, 1, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 1, 0, 1, 0, 0, 0, 1, 1, 0, 1, 1, 1, 0, 1, 1, 0, 0, 0, 1, 1, 1, 0, 1, 0, 0, 0, 1, 0, 1, 1]
+#for this case, the count or cast "shadow" is random and unrelated to the mask
 count = []
+#Contains our extrapolated mask
+extrapMask = []
 
-#Fills our count array randomly. 10 signifies no x-ray hits
-#100 signifies an x-ray hit
-for x in range(len(mask)):
-    pick = random.choice(range(0, 2))
-    if pick == 0:
-        count.append(10)
+#Extrapolates our mask
+for x in range(400):
+    indexOfMask = int(x * 70 / 400)
+    if(indexOfMask == 0):
+        indexOfMask = 1
+    if mask[indexOfMask] == 0:
+        extrapMask.append(10)
     else:
+        extrapMask.append(100)
+
+for x in range(1, 400):
+    pick = random.choice(range(0, 2))
+    if pick == 1:
         count.append(100)
+    else:
+        count.append(10)
 
 #Our cross correlation array
-cc = np.correlate(mask, count, "full")
+cc = np.correlate(extrapMask, count, "full")
 
-#Used only for the x axis of our mask and count graphs
-xaxis = []
-for x in range(len(mask)):
-    xaxis.append(str(x + 1))
-
-#Used only for the x axis of our cross correlation graph
-xaxiscc = []
-for x in range(len(cc)):
-    xaxiscc.append(str(x + 1))
+#X axes of graphs
+xaxisForMask = np.arange(0, len(mask), 1)
+xaxisForCount = np.arange(0, len(count), 1)
+xaxiscc = np.arange(0, len(cc), 1)
 
 #Everything after this is plotting our graphs
 figure, axis = plt.subplots(3)
 
 plt.rc_context({'axes.autolimit_mode': 'round_numbers'})
 
-axis[0].bar(xaxis, mask)
+axis[0].bar(xaxisForMask, mask)
 axis[0].set_title("Mask")
 
-axis[1].bar(xaxis, count)
+axis[1].bar(xaxisForCount, count)
 axis[1].set_title("Random Counts")
 
 axis[2].bar(xaxiscc, cc)
